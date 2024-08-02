21e07e4b322969831f292d35996a2b69a0eeb048
migueldialpad <miguel.navarro@dialpad.com>
2024-08-02 02:32:03 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      ```html
-      <div role="banner">
-        <span role="heading" aria-level="1">
-          Three words
-        </span>
-        <div role="navigation">
-          <a>one word</a>
-          <a>one word</a>
-          <a>one word</a>
-          <a>one word</a>
-        </div>
-      </div>
-      ```
+     
     </main>
   );
 }
